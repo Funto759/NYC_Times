5e8108c9a0832f0ad15d77df345a855a025a2910
Funto759 <funtosuru@gmal.com>
2025-02-23 09:07:36 +0100

    feat: Enhance book details display with images and improved layout
    This commit enhances the `NYCarticlesDetailsScreen` to display book images and improves the layout for better readability.
    
    **Key Changes:**
    
    -   **UI:**
        -   Added `AsyncImage` to display book images from the `book_image` URL.
        -   Refactored the layout within `OutlinedCard` to use `Row` and `Column` for better arrangement of elements.
        -   Added `Text` to display book title and book description.
        - Added `Spacer` to adjust the space between image and text.
        - Added `maxLines` and `overflow` parameter to `Text` to display only two lines and `Ellipsis` overflow.
    -   -   Updated `OutlinedCard` to be higher.
    +   -   Updated `OutlinedCard` to be higher and take all the available width.
    -   - Updated the `OutlinedCard` to be 130dp.
    -   - Added `maxLines` and `overflow` parameters to the `Text`
    -   -   Added `AsyncImage` to the list of books to display a book icon.
    +   - Added a title to describe each text view.
    -   - Added `AsyncImage` to the `NYCarticlesDetailsScreen`.

--- app/src/main/java/com/example/nyctimes/ui/NYCarticlesDetailsScreen.kt
+++ app/src/main/java/com/example/nyctimes/ui/NYCarticlesDetailsScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -39,11 +41,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import androidx.room.util.TableInfo
+import coil.compose.AsyncImage
 import com.example.nyctimes.util.Constants
 import com.example.nyctimes.model.NYCviewModel
 import com.example.nyctimes.R
@@ -87,14 +92,33 @@ fun NYCarticlesDetailsScreen(
                                 ),
                                 border = BorderStroke(1.dp, Color.Black),
                                 modifier = Modifier
-                                    .fillMaxWidth().height(100.dp)
+                                    .fillMaxWidth().height(130.dp)
                             ) {
-                                Text(
-                                    text = it.description,
-                                    modifier = Modifier
-                                        .padding(16.dp),
-                                    textAlign = TextAlign.Center,
-                                )
+                                Row(modifier = Modifier
+                                    .padding(4.dp).fillMaxSize()){
+                                    AsyncImage(
+                                        model = it.book_image,
+                                        contentDescription = "book_icon",
+                                        Modifier.padding(10.dp)
+                                    )
+                                    Spacer(Modifier.width(10.dp))
+                                    Column(Modifier.align(Alignment.CenterVertically).fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+                                        Text("Title", textAlign = TextAlign.Center,  modifier = Modifier
+                                            .padding(1.dp))
+                                        Text(text = it.title,textAlign = TextAlign.Center,  modifier = Modifier
+                                            .padding(1.dp))
+                                        Text(text = "Description",textAlign = TextAlign.Center,  modifier = Modifier
+                                            .padding(1.dp))
+                                    Text(
+                                        text = it.description,
+                                        modifier = Modifier
+                                            .padding(2.dp),
+                                        textAlign = TextAlign.Center,
+                                        maxLines = 2,
+                                        overflow = TextOverflow.Ellipsis
+                                    )
+                                    }
+                                }
                             }
                         }
                     }
